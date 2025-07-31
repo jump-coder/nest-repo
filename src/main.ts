85d53a7c094af885d66aa8ed8ad96312d8b30c67
admin <admin@localhost.localdomain>
2025-07-31 10:12:33 +0800

    新增部署deploy.yml文件实现railway部署

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,17 @@ import { ValidationPipe } from '@nestjs/common';
 import { LoggerService } from './core/logger/logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{
+  const app = await NestFactory.create(AppModule, {
     bufferLogs: true, // 使用自定义logger
   });
   app.useLogger(app.get(LoggerService))
   app.use(helmet()) // 使用helmet中间件，隐藏某些容易有安全问题的响应头
-   // 添加全局验证管道
+  // 添加全局验证管道
   app.useGlobalPipes(new ValidationPipe({
-    whitelist:true, // 启用白名单，未声明的属性舍弃
+    whitelist: true, // 启用白名单，未声明的属性舍弃
   }));
-  await app.listen(process.env.PORT ?? 3001);
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+  const host = `0.0.0.0`;
+  await app.listen(port, host);
 }
 bootstrap();
